96702eb4ada95bafbba61ced48b7b127e75c4ad8
radish@MBA2020 <ohde.radish@gmail.com>
2023-12-11 22:01:07 +0900

    Define whole layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 
+import { Box } from "@mui/material";
+
 import init, { greet } from "../momonga/pkg/momonga";
 
 function App() {
@@ -8,15 +10,39 @@ function App() {
   }, []);
 
   return (
-    <>
-      <button
-        onClick={() => {
-          greet("WebAssembly");
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+        width: "92%",
+        maxWidth: "1280px",
+        margin: "0 auto",
+      }}
+    >
+      <Box component="header" sx={{ backgroundColor: "lightyellow" }}>
+        header
+      </Box>
+      <Box
+        component="main"
+        sx={{
+          height: "100%",
+          overflow: "hidden",
+          backgroundColor: "lightblue",
         }}
       >
-        greet
-      </button>
-    </>
+        <button
+          onClick={() => {
+            greet("WebAssembly");
+          }}
+        >
+          greet
+        </button>
+      </Box>
+      <Box component="footer" sx={{ backgroundColor: "lightyellow" }}>
+        footer
+      </Box>
+    </Box>
   );
 }
 
