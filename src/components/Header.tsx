ae2ce6d2e00b806bcf3359fa2c3619e4a701a9f7
radish@MBA2020 <ohde.radish@gmail.com>
2023-12-12 04:52:22 +0900

    Define Header layout and place empty parts

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,70 @@
-import { Box } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  FormControl,
+  IconButton,
+  InputLabel,
+  MenuItem,
+  Select,
+  Toolbar,
+} from "@mui/material";
+import { IoMdDocument } from "react-icons/io";
+import { MdDarkMode } from "react-icons/md";
+import { VscLayoutPanelOff } from "react-icons/vsc";
 
 export const Header = () => {
   return (
-    <Box component="header" sx={{ backgroundColor: "lightyellow" }}>
-      header
-    </Box>
+    <AppBar position="static" sx={{ padding: "0.5rem" }}>
+      <Toolbar variant="dense">
+        <Box
+          sx={{
+            marginRight: "auto",
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            gap: "1rem",
+          }}
+        >
+          <Button variant="contained">Run</Button>
+          <FormControl>
+            <InputLabel id="code-snippets-select-label">
+              Code Snippets
+            </InputLabel>
+            <Select
+              labelId="code-snippets-select-label"
+              id="code-snippets-select"
+              label="Code Snippets"
+            >
+              <MenuItem>Item1</MenuItem>
+              <MenuItem>Item2</MenuItem>
+              <MenuItem>Item3</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+        <Box
+          sx={{
+            marginLeft: "auto",
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Button
+            startIcon={<IoMdDocument />}
+            color="inherit"
+            sx={{ borderRight: 0 }}
+          >
+            Grammar
+          </Button>
+          <IconButton>
+            <VscLayoutPanelOff />
+          </IconButton>
+          <IconButton>
+            <MdDarkMode />
+          </IconButton>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
