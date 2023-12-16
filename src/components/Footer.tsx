7692315269448968668a29486fd8742a3fc9865b
radish@MBA2020 <ohde.radish@gmail.com>
2023-12-16 20:27:23 +0900

    Fill content and apply styles to Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,25 @@
-import { Box } from "@mui/material";
+import { Box, Link } from "@mui/material";
+import { FaGithub } from "react-icons/fa";
 
 export const Footer = () => {
   return (
-    <Box component="footer" sx={{ backgroundColor: "lightyellow" }}>
-      footer
-    </Box>
+    <footer>
+      <Box
+        sx={{
+          display: "flex",
+          padding: "0.5rem",
+          alignItems: "center",
+        }}
+      >
+        <FaGithub style={{ marginRight: "0.5rem" }} />
+        <Link
+          href={import.meta.env.VITE_APP_GITHUB_REPO_URL}
+          underline="none"
+          color="inherit"
+        >
+          <span>{import.meta.env.VITE_APP_GITHUB_REPO_NAME}</span>
+        </Link>
+      </Box>
+    </footer>
   );
 };
