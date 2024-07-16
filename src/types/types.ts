3122bde160566ebaac8f6d7ac878b405282f03ac
radish19_MBA2020 <ohde.radish@gmail.com>
2024-07-16 18:47:04 +0900

    Add `While` statement snippet

--- src/types/types.ts
+++ src/types/types.ts
@@ -23,6 +23,7 @@ export type SnippetKey =
   | "functionDeclarationAndCall"
   | "ifStatement"
   | "forStatement"
+  | "whileStatement"
   | "blockScope"
   | "lexicalScoping"
   | "recursiveFunction";
